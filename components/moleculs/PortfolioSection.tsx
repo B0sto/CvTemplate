0e4e452ec9d8b520e02148f3cf05ee9b4413f56f
Bosto <bostoganashvilinika2019@gmail.com>
2025-04-19 12:05:32 +0400

    added all/code/ui comp

--- components/moleculs/PortfolioSection.tsx
+++ components/moleculs/PortfolioSection.tsx
@@ -6,9 +6,21 @@ import PortfolioProject from '../atoms/PortfolioProject'
 const PortfolioSection = () => {
   return (
     <section className='flex flex-col gap-y-[40px]'>
-        <SectionHeader header='Portfolio'/>      
-         
-         <PortfolioProject imageMain='portfolioimg1.jpg' title='Some text' description='Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo.' resourceUrl='#' imageSecondary='portfolioimg2.jpg'/>
+      <SectionHeader header='Portfolio' />
+
+      <div>
+        <div className="choose flex gap-x-[3px] pb-[20px]">
+          <p className='font-opensans text-[#26C17E] leading-[19.2px] cursor-pointer'>All</p>
+          <p className='leading-[19.2px] font-opensans'>/</p>
+          <p className='font-opensans text-[#222935] leading-[19.2px] cursor-pointer'>Code</p>
+          <p className='leading-[19.2px] font-opensans'>/</p>
+          <p className='font-opensans text-[#26C17E] leading-[19.2px] cursor-pointer'>UI</p>
+        </div>
+
+
+        <PortfolioProject imageMain='portfolioimg1.jpg' title='Some text' description='Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo.' resourceUrl='#' imageSecondary='portfolioimg2.jpg' />
+      </div>
+
     </section>
   )
 }
